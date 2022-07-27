03ca61a9db65fb89b94198c77e0a33f15ee0bfa3
brunoconde07 <brunoconde.ufpa@gmail.com>
2022-07-27 12:43:26 -0300

    [mastering-grails] -> class 13

---
+++ mastering-grails/Rewards/grails-app/controllers/rewards/InventoryController.groovy
@@ -0,0 +1,11 @@
+package rewards
+
+class InventoryController {
+    def index() {
+        render "Here is a list of everything"
+    }
+
+    def remove() {
+        render "Remove one register"
+    }
+}
